92ea4050823e2ce35b871a531f4ac52507b55392
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-01-24 05:30:44 -0800

    Add state for isPlaying

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,9 +4,8 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({song , i}) => {
+const SongCard = ({song ,isPlaying ,activeSong, data, i}) => {
 
-  const activeSong = 'Test';
 
   return (
     <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
@@ -14,6 +13,8 @@ const SongCard = ({song , i}) => {
         <div className= {`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
           ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden' }`}>
             <PlayPause 
+              isPlaying = {isPlaying}
+              activeSong = {activeSong}
               song={song}
               handlePause={handlePauseClick}
               handlePlay={handlePlayClick}
