697f1bf1e50e32fd311922fd8534cd586835b9bd
Aashray Bhandari <aashraybhandari@Aashrays-MacBook-Air.local>
2023-03-18 15:43:11 -0700

    Added Play Pause functionality to the player

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,9 +1,16 @@
 import {FaPauseCircle, FaPlayCircle} from 'react-icons/fa';
 
 const PlayPause = ({isPlaying, activeSong, song, handlePause,handlePlay}) => (
+  
   isPlaying && activeSong?.title === song.title ?
-  (<FaPauseCircle></FaPauseCircle>):
-  (<FaPlayCircle></FaPlayCircle>)
+  (<FaPauseCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePause}></FaPauseCircle>):
+  (<FaPlayCircle
+    size={35}
+    className="text-gray-300"
+    onClick={handlePlay}></FaPlayCircle>)
 );
 
 export default PlayPause;
